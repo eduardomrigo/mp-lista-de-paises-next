8f44b65336bd43938e2b24aa1af147296d114a6d
Eduardo Rigo <eduardorigom@gmail.com>
2024-06-02 17:46:32 -0300

    fix: font bug on build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
 
-const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Lista de Países",
@@ -17,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={nunitoSans.className}>
+      <body >
         <main className="bg-gray-100 min-h-screen flex flex-col items-center">
           <nav className="w-full bg-white h-16 flex items-center justify-center">
             <section className="container gap-2 flex items-center">
